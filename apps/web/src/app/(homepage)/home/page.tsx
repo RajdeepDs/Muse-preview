d7a33ab0d0844c0fb80c09987782d2d65e631b4c
RajdeepDs <rajdeepds626@gmail.com>
2024-02-16 13:22:54 +0530

    feat(homepage): add cta-section

--- apps/web/src/app/(homepage)/home/page.tsx
+++ apps/web/src/app/(homepage)/home/page.tsx
@@ -1,3 +1,4 @@
+import CTASection from "@/components/homepage/cta-section";
 import FeaturesSection from "@/components/homepage/features-section";
 import HeroSection from "@/components/homepage/hero-section";
 import OpenSourceSection from "@/components/homepage/open-source-section";
@@ -10,6 +11,10 @@ export default function HomePage(): JSX.Element {
       <div className="border-grey/10 mt-52 border-y">
         <OpenSourceSection />
       </div>
+      <div className="my-24">
+        <CTASection />
+      </div>
+      <div className="border-gradient container mb-16 h-[1px] w-full" />
     </div>
   );
 }
